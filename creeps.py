67960cd204778f3ee6f36b9ac69a285f7af525e7
facha <linuxremote@gmail.com>
2010-10-09 15:38:58 +0200

    modelview working

--- creeps.py
+++ creeps.py
@@ -11,86 +11,79 @@ class Pos:
         self.x = pos[0]
         self.y = pos[1]
 
+class MapModel:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.objects = []
+
+    def addObject(self, obj):
+        self.objects.append(obj)
+
+    def update(self):
+        for obj in self.objects:
+            obj.update()
+            if obj.pos.x < obj.size:
+                obj.pos.x = obj.size
+                obj.direction = 180 - obj.direction
+            elif obj.pos.x > self.width - obj.size:
+                obj.pos.x = self.width - obj.size
+                obj.direction = 180 - obj.direction
+            elif obj.pos.y < obj.size:
+                obj.pos.y = obj.size
+                obj.direction = -obj.direction
+            elif obj.pos.y > self.height - obj.size:
+                obj.pos.y = self.height - obj.size
+                obj.direction = -obj.direction
+
 class CreepModel:
     def __init__(self, size, init_position, init_direction, speed):
-        self.size = size
-        self.pos = Pos(init_position)
-        self.direction = init_direction
-        self.speed = speed
+        self.name = 'creep'
+        self.size = size #Creep Radius
+        self.pos = Pos(init_position) #Creep position on map
+        self.direction = init_direction #Creep direction (in degrees)
+        self.speed = speed #Creep speed
 
     def update(self):
         self.direction = self.direction % 360
         self.pos.x = self.pos.x + cos(radians(self.direction))*self.speed
         self.pos.y = self.pos.y + sin(radians(self.direction))*self.speed
-        if self.pos.x < bounds_rect.left:
-            self.pos.x = bounds_rect.left
-            self.direction = 180 - self.direction
-        elif self.pos.x > bounds_rect.right:
-            self.pos.x = bounds_rect.right
-            self.direction = 180 - self.direction
-        elif self.pos.y < bounds_rect.top:
-            self.pos.y = bounds_rect.top
-            self.direction = -self.direction
-        elif self.pos.y > bounds_rect.bottom:
-            self.pos.y = bounds_rect.bottom
-            self.direction = -self.direction
 
-class Creep(Sprite):
-    def __init__(self, screen, filename, init_position, init_direction, speed):
+class MapView(Sprite):
+    def __init__(self, screen, model):
         Sprite.__init__(self)
         self.screen = screen
-        self.base_image = pygame.image.load(filename).convert_alpha()
+        self.model = model
+        self.base_image = pygame.image.load('graycreep.png').convert_alpha()
         self.image = self.base_image
-
-        self.pos = Pos(init_position)
-        self.direction = init_direction
-        self.speed = speed
+        self.image_w, self.image_h = self.image.get_size()
 
     def update(self):
-        self.direction = self.direction % 360
-        self.image = pygame.transform.rotate(self.base_image, -self.direction)
+        pass
         
-        self.pos.x = self.pos.x + cos(radians(self.direction))*self.speed
-        self.pos.y = self.pos.y + sin(radians(self.direction))*self.speed
-        
-        self.image_w, self.image_h = self.image.get_size()
-        bounds_rect = self.screen.get_rect().inflate(-self.image_w, -self.image_h)
-        
-        if self.pos.x < bounds_rect.left:
-            self.pos.x = bounds_rect.left
-            self.direction = 180 - self.direction
-        elif self.pos.x > bounds_rect.right:
-            self.pos.x = bounds_rect.right
-            self.direction = 180 - self.direction
-        elif self.pos.y < bounds_rect.top:
-            self.pos.y = bounds_rect.top
-            self.direction = -self.direction
-        elif self.pos.y > bounds_rect.bottom:
-            self.pos.y = bounds_rect.bottom
-            self.direction = -self.direction
-    
     def blitme(self):
-        draw_pos = self.image.get_rect().move(
-            self.pos.x - self.image_w / 2, 
-            self.pos.y - self.image_h / 2)
-        self.screen.blit(self.image, draw_pos)
-           
+        for obj in self.model.objects:
+            draw_pos = self.image.get_rect().move(
+                obj.pos.x - self.image_w / 2, 
+                obj.pos.y - self.image_h / 2)
+            self.screen.blit(self.image, draw_pos)
+    
 def run_game():
     SCREEN_WIDTH, SCREEN_HEIGHT = 300, 400
     BG_COLOR = 150, 150, 80
     N_CREEPS = 10
     CREEP_SPEED = 2
 
+    environment = MapModel(SCREEN_WIDTH, SCREEN_HEIGHT)
+    for i in range(N_CREEPS):
+        creep = CreepModel(5 , (randint(0,SCREEN_WIDTH),randint(0,SCREEN_HEIGHT)), randint(0,360), CREEP_SPEED)
+        environment.addObject(creep)
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
     clock = pygame.time.Clock()
 
-    # Create N_CREEPS random creeps.
-    creeps = []    
-    for i in range(N_CREEPS):
-        creeps.append(Creep(screen, 'graycreep.png', 
-                            (randint(0,SCREEN_WIDTH),randint(0,SCREEN_HEIGHT)), 
-                            randint(0,360), CREEP_SPEED))
+    environment_view = MapView(screen, environment)
 
     while True:
         clock.tick(40)
@@ -101,9 +94,8 @@ def run_game():
         
         screen.fill(BG_COLOR)
         
-        for creep in creeps:
-            creep.update()
-            creep.blitme()
+        environment.update()
+        environment_view.blitme()
 
         pygame.display.flip()
 
